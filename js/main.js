34dcb15652c4a813191486a6a2eaab52829cc51f
Carlos <arimatea865@gmail.com>
2019-07-31 13:56:08 -0500

    First Class  Js

---
+++ js/main.js
@@ -0,0 +1,31 @@
+var memoryUsb = 4096;
+var photoImage = 3000;
+
+
+console.log('En la memoria de 4gb le caben: ' + memoryUsb/photoImage + ' fotos')
+
+
+//console.log('Hola Mundo from JS');
+//console.log("Los dias que he vivido son:");
+
+//tenemos una memoria usb de 4gb ¿cuantas fortos de 3mb le caben?
+
+
+/*tenemos un estacionamiento con 22 cubiculos, en cada cubiculo podemos meter 
+3 carros y 2 camionetas.¿cuantos cubiculos llenamos completos? ¿cuantas camionetas sobran?
+¿cuantos carros sobran?*/
+
+//camionetas : 57
+//carros : 130
+
+var carPark = 22;
+var totalCar = 130;
+var totalTruck = 57;
+
+var totalParkingCars = carPark * 3;
+var totalParkingTrucks = carPark * 2;
+
+console.log('Full House?' (totalParkingCars < totalCar) && (totalParkingTrucks < totalTruck))
+
+console.log(totalParkingCars - totalCar)
+console.log(totalParkingTrucks - totalTruck)
