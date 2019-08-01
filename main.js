118828492db5a04337fa80b4c0e813d231d70047
Carlos <arimatea865@gmail.com>
2019-08-01 13:33:29 -0500

    Ejercicios Variables y sentencias

---
+++ main.js
@@ -0,0 +1,70 @@
+const VALOR = 3.1416;
+
+var radioCirculo= 5**2;
+
+console.log(VALOR * radioCirculo);
+
+/*
+Variables let, var y const
+*/ 
+
+var myPet = 'Sr. Peabody'
+
+myPet
+"Sr. Peabody"
+myPet = 'Misifus'
+"Misifus"
+let myDog = 'Sr. Peabody'
+
+myDog = 'Misifus'
+"Misifus"
+const name = 'Carlos'
+
+typeof 'hola mundo'
+"string"
+0 == 0
+
+0 == false
+
+0 === false
+
+false == null
+
+false == undefined
+
+false === undefined
+
+
+/*
+Desiciones if, else if, else
+*/ 
+/*var numberOne = 1;
+var numberTwo = -10;
+
+
+if ((numberOne > 11) || (numberTwo > 11)){
+	console.log('uno de los dos es mayor a 11')
+} else if((numberOne <= 2) || (numberTwo < 2)) {
+	console.log('Uno de los dos es menor a 2');
+} else {
+  console.log('No se pudo');
+}*/
+
+//ejercicio
+
+var number = 2;
+var numberUnpair = 3;
+var numberString = 'a'
+
+if(number % 2 == 0 && typeof number === 'number'){
+    console.log('Es un entero');
+}else{
+    console.log('No es par');
+}
+
+//validar tambien cuando un numero es impar
+
+//saber si el producto de 2 numeros es primo y mayor a 40
+//validar que los numeros sean digitos
+//{numberOne}{numberTwo}
+
